05d06f49117233e377e45e4875b4ca8e18787eb3
Rodch_ot15@top-academy.ru <Rodch_ot15@top-academy.ru>
2022-11-23 21:19:45 +0300

    Recursion done

--- Recursion/Source.cpp
+++ Recursion/Source.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 void elevator(int floor);
+int factorial(int n);
+
+//#define ELEVATOR
 
 void main()
 {
 	setlocale(LC_ALL, "");
+	
+#ifdef factorial
 	cout << "Hello Recursion" << endl;
 	int n;
 	cout << "Введите номер этажа: "; cin >> n;
 	elevator(n);
+	
 }
 
 void elevator(int floor)
@@ -22,4 +28,18 @@ void elevator(int floor)
 	cout << "Вы на  " << floor << " этаже" << endl;
 	elevator(floor - 1);
 	cout << "Вы на  " << floor << " этаже" << endl;
+
+}
+#endif//ELEVATOR
+
+int n;
+cout << "Введите число для вычисления факториал: "; cin >> n;
+cout << factorial(n) << endl;
+}
+
+int factorial(int n)
+{
+	/*if (n == 0)return 1;
+	else return n * factorial(n - 1);*/
+	return n == 0 ? 1 : n * factorial(n - 1);
 }
